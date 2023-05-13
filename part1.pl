c4636074357b2389142b2ba9ab324079bd1b0b78
Totti00 <gek5800@gmail.com>
2023-05-13 13:01:30 +0200

    finish part 2

--- part1.pl
+++ part1.pl
@@ -43,25 +43,32 @@ count(List, E, N) :- count(List, E, zero, N).
 count(nil, E, N, N).
 count(cons(E, L), E, N, M) :- count(L, E, s(N), M).
 count(cons(E, L), E2, N, M) :- E \= E2, count(L, E2, N, M).
-% c
+% count(cons(a, cons(b, cons(c, cons(a, cons(b, nil))))), a, N)
 % count(cons(a, cons(b, cons(c, cons(a, cons(b, nil))))), a, zero, N)
 % count(cons(b, cons(c, cons(a, cons(b, nil)))), a, s(zero), N)
 % count(cons(c, cons(a, cons(b, nil))), a, s(zero), N)
 
 %2.4
 % max(List, Max)
-greater(s(_), zero).
+greater(N, zero).
 greater(s(N), s(M)) :- greater(N, M).
 
 max(cons(H, T), Max) :- max(T, H, Max).
 max(nil, Max, Max).
 max(cons(H, T), Temp, Max) :- greater(H, Temp), max(T, H, Max).
 max(cons(H, T), Temp, Max) :- greater(Temp, H), max(T, Temp, Max).
-%NON VA
+% max(cons(zero, cons(s(zero), cons(zero, cons(s(s(zero)), nil)))), X)
 
 %2.5
 % min-max(List, Min, Max)
+minMax(cons(H, T), Min, Max) :- minMax(T, H, H, Min, Max).
+minMax(nil, TempMin, TempMax, TempMin, TempMax).
+minMax(cons(H, T), TempMin, TempMax, Min, Max) :- greater(H, TempMax), minMax(T, TempMin, H, Min, Max).
+minMax(cons(H, T), TempMin, TempMax, Min, Max) :- greater(TempMin, H), minMax(T, H, TempMax, Min, Max).
+% minMax(cons(zero, cons(s(s(zero)), cons(s(s(s(s(zero)))), cons(zero, nil)))), Min, Max).
 
+%3.1
+% same(List1, List2)
 
 
 
